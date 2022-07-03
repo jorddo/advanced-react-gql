7e655802c0f6dc95252609692837d94b89ca496f
jorddo <jordanbsneed@gmail.com>
2022-07-03 10:49:59 -0500

    setting up product queries

--- sick-fits/frontend/components/SingleProduct.js
+++ sick-fits/frontend/components/SingleProduct.js
@@ -1,24 +1,61 @@
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
+import Head from 'next/head';
+import styled from 'styled-components';
 import DisplayError from './ErrorMessage';
 
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
 const SINGLE_ITEM_QUERY = gql`
-  query {
-    Product(where: { id: "62ba1c27e1d740565c17e861" }) {
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
       name
       price
       description
+      id
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
     }
   }
 `;
 
 export default function SingleProduct({ id }) {
-  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id,
+    },
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
+  const { Product } = data;
   return (
-    <div>
-      <h2>{data.Product.name}</h2>
-    </div>
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {Product.name}</title>
+      </Head>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </ProductStyles>
   );
 }
---
+++ sick-fits/frontend/components/UpdateProduct.js
@@ -0,0 +1,6 @@
+export default function UpdateProduct() {
+  // 1. get existing product
+  // 2. we need to get the mutation to update the product
+  // 3. we need the form to handle the updates
+  return <p>Update!!!!!</p>;
+}
